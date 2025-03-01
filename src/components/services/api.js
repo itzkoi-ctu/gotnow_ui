81f972b1542efb49941afc868e1667ff9b403d16
itz-koi <longnguyen0397042@gmail.com>
2025-03-01 11:31:26 +0700

    setting URL

--- src/components/services/api.js
+++ src/components/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { logoutUser } from '../services/authService'
-const BASE_URL= "http://localhost:8080/api/v1"
+// const BASE_URL= "http://localhost:8080/api/v1"
+const BASE_URL= "https://gotnow-api.onrender.com"
 
 export  const  privateApi= axios.create({
     baseURL: BASE_URL,
